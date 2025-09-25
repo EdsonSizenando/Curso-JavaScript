4e2d86d5ab94d75d04d5243af1ffbb3d803479b7
EdsonSizenando <edson.sizenando@gmail.com>
2025-09-24 23:45:21 -0300

    exercicio 012 JS

---
+++ aula012/ex012.js
@@ -0,0 +1,14 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas`)
+if (hora < 12  ) {
+    console.log('Bom Madrugada!')
+} else if (hora <= 18) {
+    console.log('Boa Tarde!') 
+} else if (hora <= 23) {
+    console.log('Boa Noite!')
+} else if (hora >= 0 && hora < 5 ){
+    console.log('Boa Madrugada!')
+}
+
+
