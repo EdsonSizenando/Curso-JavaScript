5532723fd8c90ddb76fbfbd79db565a02cb59cb2
EdsonSizenando <edson.sizenando@gmail.com>
2025-09-24 23:58:14 -0300

    exercicio 013 JS

---
+++ aula012/ex013.js
@@ -0,0 +1,44 @@
+agora = new Date()
+var diaSem = agora.getDay()
+/*
+0 = domingo
+1 = segunda 
+2 = terça 
+3 = quarta
+4 = quinta
+5 = sexta
+6 = sabado
+
+em swtich o break é sempre obrigatorio no fim de cada case
+
+*/
+
+
+console.log(diaSem)
+
+switch(diaSem) {
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+        console.log('Terça')
+        break
+    case 3:
+        console.log('Quarta')
+        break
+    case 4:
+        console.log('Quinta')
+        break
+    case 5:
+        console.log('Sexta')
+        break
+    case 6:
+        console.log('Sabádo')
+        break
+    default:
+        console.log('[ERRO] Dia Inválido')
+
+}
