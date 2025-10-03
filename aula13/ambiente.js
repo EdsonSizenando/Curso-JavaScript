a927cefeaaa9dc69ab11e33e1003bda118a1a15e
EdsonSizenando <edson.sizenando@gmail.com>
2025-10-03 12:07:15 -0300

    aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,33 @@
+// Estrutura de repetição com teste lógico no inicio.
+
+var c = 1   // variavel contador
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++  //    c = c + 1
+}
+
+// estrutura de repetição com teste lógico no final.
+
+var c = 1 
+do {
+    console.log(`Passo ${c}`)
+} while (c <= 6)
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ console.log('Tudo bem?')
+ console.log('Tudo bem?')
+ console.log('Tudo bem?')
+ console.log('Tudo bem?')
+ console.log('Tudo bem?')
+ console.log('Tudo bem?') */
