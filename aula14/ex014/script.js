462b5223b847ad06c52ebe136508dffcdda3815e
EdsonSizenando <edson.sizenando@gmail.com>
2025-10-05 09:18:57 -0300

    exercicio aula 14

---
+++ aula14/ex014/script.js
@@ -0,0 +1,14 @@
+function Contar() {
+    var inicio = document.getElementById('inicio')
+    var fim = document.getElementById('fim')
+    var passo = document.getElementById('passo')
+    var resp = document.getElementById('resp')
+
+    for (inicio ; fim ; passo) {
+        console.log(`${resp}`)
+    }
+       
+
+
+
+}
