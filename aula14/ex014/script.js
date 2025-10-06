a0cf50568cc83d754f7730037816a94e4162b09c
EdsonSizenando <edson.sizenando@gmail.com>
2025-10-06 15:12:19 -0300

    ex14 da aula 14

--- aula14/ex014/script.js
+++ aula14/ex014/script.js
@@ -1,14 +1,36 @@
-function Contar() {
-    var inicio = document.getElementById('inicio')
-    var fim = document.getElementById('fim')
-    var passo = document.getElementById('passo')
-    var resp = document.getElementById('resp')
+function contar() {
+   let inicio = document.getElementById('txti')
+   let fim = document.getElementById('txtf')
+   let passo = document.getElementById('txtp')
+   let res = document.getElementById('resp')
 
-    for (inicio ; fim ; passo) {
-        console.log(`${resp}`)
+   if (inicio.value.length == 0 || fim.value.lenght == 0 || passo.value.lenght == 0) {
+    res.innerHTML = 'Impossivel Contar'
+    //window.alert('[ERRO] Faltam dados!')
+   } else {
+        res.innerHTML = 'Contando... <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo Inválido! Considerando PASSO 1.')
+            p = 1
+        }
+      
+        if (i < f){
+            //contagem crescente
+            for (let c = 1 ; c <= f ; c += p) {
+                res.innerHTML += `${c}  \u{1F499}`
+            }
+            
+        } else {
+            //contagem regressiva
+            for (let c = i ; c >= f ; c -= p) {
+                res.innerHTML += `${c}  \u{1F499}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
-       
-
-
+    
 
 }
