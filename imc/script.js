acb5591b7e1a7f77f651b5d72f88359fd4dfe0e8
EdsonSizenando <edson.sizenando@gmail.com>
2025-10-13 13:34:12 -0300

    projeto-imc concluido

--- imc/script.js
+++ imc/script.js
@@ -1,30 +1,50 @@
 let altura = document.getElementById('txtalt')
 let peso = document.getElementById('txtpeso')
 let resp = document.getElementById('resp')
-
-
-
+let categoria = ['MAGREZA', 'NORMAL', 'SOBREPESO', 'OBESIDADE', 'OBESIDADE GRAVE']
 
 
 
 
 function calcular() {
+
 let imc = ( Number(peso.value) ) / (Number(altura.value) * Number(altura.value))
 let imcformat = imc.toFixed(2)
+
 
 if (Number(altura.value) == 0 && Number(peso.value) == 0 ) {
     window.alert('Valor Inválido. Digite Novamente')
 
 
-} else if (Number(imcformat) < 18) {
-    resp.innerHTML += `Classificado como MAGREZA`
-    resp.push(Text(resp.value))
-
+} else if (imcformat < 18.5) {
+    categoria = categoria[0]
     
+} else if (imcformat >= 18.5 && imcformat <= 24.9) {
+    categoria = categoria[1]
+
+} else if (imcformat >= 25 && imcformat <= 29.9) {
+    categoria = categoria[2]
+
+} else if (imcformat <=30 && imcformat <= 39.9) {
+    categoria = categoria[3]
+
+} else {
+    categoria = categoria[4]
+}
+
+
+resp.innerHTML = `O valor do seu IMC corresponde a ${imcformat} classificado como ${categoria}.`
+
 
 }
 
-resp.innerHTML = `O valor do seu IMC corresponde a ${imcformat}` 
 
+function limpar() {
+
+    resp.innerHTML = ''
+    altura.value = ''
+    peso.value = ''
+    altura.focus()
+    peso.focus()
 }
 
